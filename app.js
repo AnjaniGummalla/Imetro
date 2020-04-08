94cb304722f1bca30f48c64a01dca9c7b05281f5
AnjaniGummalla <anjani513devi@gmail.com>
2020-04-08 21:25:21 +0530

    changes done

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./auth/AuthController');
+//var indexRouter = require('./routes/index');
+var authRouter = require('./routes/AuthController');
 var app = express();
 
 //Databaseconnection
@@ -25,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', authRouter);
 
 // catch 404 and forward to error handler
--- routes/AuthController.js
+++ routes/AuthController.js
@@ -59,6 +59,38 @@ router.post('/register', function(req, res) {
 
 });
 
+router.post('/forgotpassword', async function(req, res) {
+
+       var email = req.body.email;
+         console.log(email);
+       var passworddata = await User.find({email:email}).select('password');
+       console.log("passworddata",passworddata[0].password)
+         var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'anjani513devi@gmail.com',
+            pass: 'anjanichotu4507'
+          }
+        });
+
+        var mailOptions = {
+        //from: 'anjani513devi@gmail.com',
+          to: email,
+          subject: 'Paaword Mail!!!',
+          text: "Your current password is " + passworddata[0].password
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+          console.log("erorr related the mail ", error);
+          } else {
+          console.log('Email sent: ' + info.response);
+          }
+        });
+        res.status(200).send("password has been sent to your email!!!");
+       
+        });
+
 router.put('edit/:id',VerifyToken, function (req, res) {
         User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
             if (err) return res.status(500).send("There was a problem updating the user.");
